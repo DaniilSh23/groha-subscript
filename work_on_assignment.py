c5eb19d465f62b4393d2601d05e530b542f67255
DaniilSh23 <danyasevas11@gmail.com>
2023-05-16 17:26:24 +0300

    сделаны: обработка исключений при выполнении заданий, конкретные ответы от функций со статус кодами и описанием, результаты работы потоков, записаны заготовки для обработки результатов работы потоков.

--- work_on_assignment.py
+++ work_on_assignment.py
@@ -6,7 +6,7 @@ import random
 import shutil
 import threading
 import time
-from typing import List
+from typing import List, Tuple
 import socks
 
 from telethon.errors import ChannelsTooMuchError, ChannelInvalidError, ChannelPrivateError, InviteHashEmptyError, \
@@ -20,14 +20,32 @@ from my_exceptions import MyException
 from settings.settings import BASE_DIR, MY_LOGGER
 
 
-async def do_subscription(client, target, thread_id):
+async def do_subscription(client, target, thread_id) -> Tuple[int, str]:
     """
     Выполняем подписку на нужный канал или группу в Telegram.
     client - объект клиента telethon
     target - идентификатор(ссылка, юзернейм и т.п.) канала, на который надо подписаться
     thread_id - номер потока
+
+    Коды возвратов этой функции:
+        Базовые коды:
+            1 - все прошло хорошо, аккаунт успешно подписался на канал / группу
+            2 - неожиданное исключение, которое не обрабатывается классами telethon
+        Коды о проблемах с каналом(чатом):
+            3 - в ссылке отсутствует хэш, необходимый для подписки
+            4 - пригласительная ссылка просрочена
+            5 - неверная пригласительная ссылка
+            8 - превышено максимальное количество пользователей в данном чате
+            11 - недопустимый тип канала (чата), возможно проблема в ссылке
+        Коды о проблемах с аккаунтом:
+            6 - аккаунт уже присоединён к большому количеству каналов (групп)
+            7 - включена 2ФА и требуется пароль
+            9 - пользователь уже является участником данного канала (чата)
+            10 - аккаунт уже вступил в слишком большое количество каналов (чатов)
+            12 - юзер быд забанен в канале (чате) или канал приватный и у юзера нет прав для вступления
+
+    :return (code, description)
     """
-    # TODO: доработать...
 
     if target.split('/')[-1].startswith('+'):
 
@@ -38,17 +56,20 @@ async def do_subscription(client, target, thread_id):
         try:
             await client(functions.messages.CheckChatInviteRequest(hash=lnk_hash))
         except InviteHashEmptyError as err:
-            MY_LOGGER.warning(f'В ссылке отсутсвует хэш, необходимый для подписки! '
+            MY_LOGGER.warning(f'В ссылке отсутствует хэш, необходимый для подписки! '
                               f'Ссылка: {target}. Текст ошибки: {err}')
-            return
+            return 3, str(err)
         except InviteHashExpiredError as err:
             MY_LOGGER.warning(f'Ссылка-приглашение просрочена! '
                               f'Ссылка: {target}. Текст ошибки: {err}')
-            return
+            return 4, str(err)
         except InviteHashInvalidError as err:
             MY_LOGGER.warning(f'Неверная ссылка-приглашение! '
                               f'Ссылка: {target}. Текст ошибки: {err}')
-            return
+            return 5, str(err)
+        except Exception as err:
+            MY_LOGGER.error(f'В процессе проверке ссылки-приглашения получена неожиданная ошибка. Текст ошибки: {err}')
+            return 2, str(err)
 
         MY_LOGGER.debug(f'Пробуем вступить в канал по ссылке: {target}')
         try:
@@ -56,34 +77,34 @@ async def do_subscription(client, target, thread_id):
         except ChannelsTooMuchError as err:
             MY_LOGGER.warning(f'Аккаунт уже присоединён к большому кол-ву каналов/групп! '
                               f'Текст ошибки: {err}')
-            return
+            return 6, str(err)
         except InviteHashEmptyError as err:
-            MY_LOGGER.warning(f'В ссылке отсутсвует хэш, необходимый для подписки! '
+            MY_LOGGER.warning(f'В ссылке отсутствует хэш, необходимый для подписки! '
                               f'Ссылка: {target}. Текст ошибки: {err}')
-            return
+            return 3, str(err)
         except InviteHashExpiredError as err:
             MY_LOGGER.warning(f'Ссылка-приглашение просрочена! '
                               f'Ссылка: {target}. Текст ошибки: {err}')
-            return
+            return 4, str(err)
         except InviteHashInvalidError as err:
             MY_LOGGER.warning(f'Неверная ссылка-приглашение! '
                               f'Ссылка: {target}. Текст ошибки: {err}')
-            return
+            return 5, str(err)
         except SessionPasswordNeededError as err:
             MY_LOGGER.warning(f'Включена 2-ФА и требуется пароль.! '
                               f'Ссылка: {target}. Текст ошибки: {err}')
-            return
+            return 7, str(err)
         except UsersTooMuchError as err:
             MY_LOGGER.warning(f'Превышено максимальное кол-во пользователей в данном чате! '
                               f'Ссылка: {target}. Текст ошибки: {err}')
-            return
+            return 8, str(err)
         except UserAlreadyParticipantError as err:
             MY_LOGGER.warning(f'Пользователь уже является участником канала/чата! '
                               f'Ссылка: {target}. Текст ошибки: {err}')
-            return
+            return 9, str(err)
         except Exception as err:
             MY_LOGGER.error(f'Не удалось подписаться на канал/чат: {target}. Текст ошибки: {err}')
-            return
+            return 2, str(err)
 
     else:
         MY_LOGGER.debug(f'Поток № {thread_id}\tДля подписки передан приватный канал/чат.')
@@ -95,27 +116,27 @@ async def do_subscription(client, target, thread_id):
         except ChannelsTooMuchError as err:
             MY_LOGGER.warning(f'Не удалось подписаться на канал: {target}. '
                               f'Причина: аккаунт присоединился к слишком большому количеству каналов. Текст ошибки: {err}')
-            return
+            return 10, str(err)
         except ChannelInvalidError as err:
             MY_LOGGER.warning(f'Не удалось подписаться на канал: {target}. '
                               f'Причина: недопустимый тип канала, убедитесь, что Вы передаёте правильный идентификатор '
                               f'канала. Текст ошибки: {err}')
-            return
+            return 11, str(err)
         except ChannelPrivateError as err:
             MY_LOGGER.warning(f'Не удалось подписаться на канал: {target}. '
                               f'Причина: канал является приватным и у аккаунта нет прав, или аккаунт был '
                               f'забанен в канале/чате. Текст ошибки: {err}')
-            return
+            return 12, str(err)
         except Exception as err:
             MY_LOGGER.error(f'Не удалось подписаться на канал: {target}. Текст ошибки: {err}')
-            return
+            return 2, str(err)
 
     MY_LOGGER.success(f'Поток № {thread_id}\tАккаунт успешно подписался на канал {target}')
-    return True
+    return 1, 'all right'
 
 
-def worker(proxy_string: str, time_auth_proxy: str, time_auth_accounts: str, reset_accounts: str, thread_id: int, thread_accs: List[str],
-           target: str):
+def worker(proxy_string: str, time_auth_proxy: str, time_auth_accounts: str, reset_accounts: str, thread_id: int,
+           thread_accs: List[str], target: str) -> Tuple[int, str]:
     """
     Работа по заданию в потоке.
         proxy_string - список проксей в виде строки,
@@ -124,6 +145,14 @@ def worker(proxy_string: str, time_auth_proxy: str, time_auth_accounts: str, res
         thread_id - номер потока,
         thread_accs - список аккаунтов для данного потока (название файла без расширения)
         target - username или ссылка на канал, на который подписываемся
+
+    Коды возврата потока:
+        1 - Успешная отработка по заданию
+        2 - Во время выполнения задания возникло неожиданное исключение
+        3 - Во время работы по заданию возникла проблема с каналом(группой) на которую надо подписаться
+        (проблема в задании)
+
+    :return (code, description)
     """
     MY_LOGGER.info(f'Поток № {thread_id}\tПолучил в работу {len(thread_accs)} аккаунтов.')
 
@@ -155,6 +184,7 @@ def worker(proxy_string: str, time_auth_proxy: str, time_auth_accounts: str, res
             # Добавляем проксю в общий список
             proxy_lst.append(proxy_dct)
 
+    success_accs = 0    # успешно отработавшие аккаунты
     for i_indx, i_acc in enumerate(thread_accs):
         MY_LOGGER.info(f'Поток № {thread_id}\tБерёт в работу {i_indx + 1} аккаунт '
                        f'из {len(thread_accs)} аккаунтов в потоке.')
@@ -197,7 +227,7 @@ def worker(proxy_string: str, time_auth_proxy: str, time_auth_accounts: str, res
                 MY_LOGGER.debug(f'Поток № {thread_id}\tРезультат работы акка {i_acc!r} == {rslt}')
 
                 # Перемещение файлов для аккаунта в папку done
-                if rslt:
+                if rslt[0] == 1:
                     MY_LOGGER.debug(f'Поток № {thread_id}\tПеремещаем аккаунт {i_acc!r} в папку "done"')
                     done_dir = os.path.join(BASE_DIR, 'done')
                     if not os.path.exists(done_dir):
@@ -211,12 +241,23 @@ def worker(proxy_string: str, time_auth_proxy: str, time_auth_accounts: str, res
                         src=os.path.join(BASE_DIR, 'accounts', f'{i_acc}.json'),
                         dst=os.path.join(BASE_DIR, 'done', f'{i_acc}.json')
                     )
+                    success_accs += 1
+
+                if rslt[0] == 2:
+                    MY_LOGGER.debug(f'Поток № {thread_id}\tПолучен код ответа 2 от функции подписки. '
+                                    f'Поток будет остановлен.')
+                    return 2, rslt[1]
+
+                elif rslt[0] in (3, 4, 5, 8, 11):
+                    MY_LOGGER.debug(f'Поток № {thread_id}\tПроблема с каналом, на который необходимо подписаться. '
+                                    f'Поток будет остановлен')
+                    return 3, rslt[1]
 
         except ConnectionError as err:
             MY_LOGGER.warning(f'Поток № {thread_id}\tАккаунту {i_acc} не удалось подключится к прокси {rand_proxy}. '
                               f'Текст ошибки: {err}')
 
-            # Перемещение файлов аккаунта, которому не удалось подключится к проксе в папку no_connect
+            # Перемещение файлов аккаунта, которому не удалось подключиться к проксе в папку no_connect
             no_connect_dir = os.path.join(BASE_DIR, 'no_connect')
             if not os.path.exists(no_connect_dir):
                 MY_LOGGER.debug(f'Поток № {thread_id}\tПапка no_connect отсутствует, создаём её.')
@@ -232,10 +273,14 @@ def worker(proxy_string: str, time_auth_proxy: str, time_auth_accounts: str, res
             MY_LOGGER.debug(f'Поток № {thread_id}\tРассчитываем таймаут между действиями аккаунтов. '
                             f'Диапазон паузы: {time_auth_accounts}')
             acc_timeout = time_auth_accounts.replace(' ', '').split('-')
-            sleep_betwen_actions = random.randint(int(acc_timeout[0]), int(acc_timeout[1]))
+            sleep_between_actions = random.randint(int(acc_timeout[0]), int(acc_timeout[1]))
             MY_LOGGER.info(f'Поток № {thread_id}\tПауза перед действием для следующего аккаунта: '
-                           f'{sleep_betwen_actions} сук.')
-            time.sleep(sleep_betwen_actions)
+                           f'{sleep_between_actions} сук.')
+            time.sleep(sleep_between_actions)
+
+        main_result = 1, f'{success_accs}|{len(thread_accs)}'
+        MY_LOGGER.info(f'Поток № {thread_id}\tОтработал. Успешно подписались {success_accs} из {len(thread_accs)}')
+        return main_result
 
 
 def main_work_on_assignment(limits_dct: dict, target: str):
@@ -288,12 +333,28 @@ def main_work_on_assignment(limits_dct: dict, target: str):
     for i_thread_id in range(1, int(threads_numb) + 1):
         MY_LOGGER.info(f'Запускаем поток № {i_thread_id}')
         i_thread_accs = threads_acc_dct.get(i_thread_id)
-        thread = threading.Thread(target=worker, args=(
-            proxy_string, time_auth_proxy, time_auth_accounts,
-            reset_accounts, i_thread_id, i_thread_accs, target
-        ))
-        threads.append(thread)
+        thread = threading.Thread(target=lambda: setattr(threading.current_thread(), 'result', worker(
+            proxy_string, time_auth_proxy, time_auth_accounts, reset_accounts, i_thread_id, i_thread_accs, target
+        )))
         thread.start()
+        threads.append(thread)
 
+    work_rslt = [0, 0]  # [Кол-во успешно подписанных аккаунтов, общее кол-во аккаунтов]
     for i_thread in threads:
         i_thread.join()
+        thread_result = i_thread.result
+
+        if thread_result[0] == 1:
+            MY_LOGGER.debug(f'Плюсуем результат работы потока к успешно отработавшим аккаунтам и общему их числу')
+            work_rslt[0] += thread_result[1].split('|')[0]
+            work_rslt[1] += thread_result[1].split('|')[1]
+
+        elif thread_result[0] == 2:
+            # TODO: обработка для неожиданного исключения в потоке
+            pass
+
+        elif thread_result[0] == 2:
+            # TODO: обработка для хренового задания, полученного от сервиса (учесть также какой был выбран сервис)
+            pass
+
+
